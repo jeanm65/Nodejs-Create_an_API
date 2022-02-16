0d8d1e24fac9139d3360c975bf51ed1cc8df5662
Jean Marie <jmrandrianant@gmail.com>
2022-02-16 18:02:21 +0300

    postControllers CRUD, use of POSTMAN, CRUD with POSTMA,etc

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express();
-require('./models/dbConfig')
+require('./models/dbConfig') // we need the dbconfig in our index.js file
+const bodyParser = require('body-parser')
+
+const postsRoutes = require('./routes/postsController') // we want that our route (controller) is only accessible in our index.js file
+ app.use(bodyParser.json())
+
+app.use('/posts', postsRoutes);
+
+
 
 app.listen(5500, () => {
   console.log('server started: 5500')
---
+++ routes/postsController.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId;
+
+const{PostsModel} = require('../models/postsModel')
+
+router.get('/', (req, res) => {
+  PostsModel.find((err, docs) => {
+    if(!err) res.send(docs);
+    else  (console.log('error to get data:' + err))
+  })
+}) 
+
+// send data with POST
+router.post('/', (req, res) => {
+//console.log(req);find all about the req parameter
+  const newRecord = new PostsModel({
+      author: req.body.author, // req.body only work with body-parser
+      message: req.body.message
+  });
+
+  newRecord.save((err, docs) => {
+    if(!err) res.send(docs);
+    else console.log('error creating new data : ' + err)
+  })
+})
+
+// update with PUT
+router.put('/:id', (req, res) => {
+    // if the id is unknown, should be protected from that: 
+  if(!ObjectId.isValid(req.params.id))
+    return res.status(400).send('ID unknown:' + req.params.id)
+
+  const updateRecord = {
+      author: req.body.author,
+      message: req.body.message
+  };
+
+  PostsModel.findByIdAndUpdate(
+      req.params.id,
+      {$set:updateRecord},
+      {new:true},
+      (err, docs) => {
+        if(!err) res.send(docs);
+        else console.log('update error: ' + err)
+      }
+  )
+})
+ 
+module.exports = router
